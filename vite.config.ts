ef819f4c94b2264c5909328db1d5e4b9e2c99b15
antonioseixasdev <antonioseixas@sxsconsultoria.com.br>
2025-05-17 14:43:09 -0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/conecta-consultoria-web/', // Adicione esta linha para GitHub Pages
   server: {
     host: "::",
     port: 8080,
