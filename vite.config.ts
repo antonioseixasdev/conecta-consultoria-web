b306fe5b740c0733a7abe2874bf0fd2509afd4c7
antonioseixasdev <antonioseixas@sxsconsultoria.com.br>
2025-05-17 16:13:37 -0300

    Ajuste no vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,26 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/conecta-consultoria-web/', // Adicione esta linha para GitHub Pages
+  base: '/conecta-consultoria-web/',
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'dist/index.html',
+          dest: '', // copia para dist/404.html
+          rename: '404.html'
+        }
+      ]
+    })
   ].filter(Boolean),
   resolve: {
     alias: {
