3132d45be6f2f9e36b217e2a0df0ae44a391743b
antonioseixasdev <antonioseixas@sxsconsultoria.com.br>
2025-06-18 18:27:43 -0300

    Preparando deploy para o Firebase Hosting

--- chamarWebhook.js
+++ chamarWebhook.js
@@ -1,9 +1,13 @@
 import fetch from 'node-fetch';
 
+const usuario = 'botsxs';
+const senha = 'Jo@o1911.';
+
 fetch('https://sxsconsultoria.app.n8n.cloud/webhook/e35b16d3-bb8d-44a5-9294-2982cfb286c7', {
   method: 'GET',
   headers: {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    'Authorization': 'Basic ' + Buffer.from(`${usuario}:${senha}`).toString('base64')
   }
 })
   .then(res => res.text())
